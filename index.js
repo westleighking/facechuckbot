80d360721ba0b2fa34c7e5233388ed02e1c41674
Westleigh King <31257256+westleighking@users.noreply.github.com>
2021-05-16 15:39:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.status(200).send(`SERVER IS WORKING!`)
+    res.status(200).send(`SERVER IS WORKING!`, port)
     console.log(req, res);
 })
 
