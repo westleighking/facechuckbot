eb192ee00174e10ddd91946a602c0f3a94e4b7ea
Westleigh King <31257256+westleighking@users.noreply.github.com>
2021-05-16 15:33:01 -0400

    pushed to see api response

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('https://api.icndb.com/jokes/random/', (req, res) => {
-	res.status(200).send('Server is working.')
+	res.status(200).send(JSON.stringify(res))
 })
 
 
